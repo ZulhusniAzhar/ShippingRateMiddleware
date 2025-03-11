5dc7d0fc01e9da1afa5ac8511b0a688d82a6cac4
Zulhusni Azhar <92666477+ZulhusniAzhar@users.noreply.github.com>
2025-03-12 03:12:16 +0800

    Secure Token and Values

--- src/main/java/com/SPL_middleware/assignment/config/FeignConfig.java
+++ src/main/java/com/SPL_middleware/assignment/config/FeignConfig.java
@@ -1,10 +1,8 @@
 package com.SPL_middleware.assignment.config;
-
-
 import feign.RequestInterceptor;
-import feign.okhttp.OkHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,26 +11,23 @@ public class FeignConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(FeignConfig.class);
 
+    @Value("${feign.request.origin}")
+    private String origin;
+
+    @Value("${feign.request.x-requested-with}")
+    private String xRequestedWith;
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
-            // Add missing headers since I cant set it up using @Header in the Feign Interface
-            requestTemplate.header("Origin", "https://www.jtexpress.my");
-            requestTemplate.header("X-Requested-With", "XMLHttpRequest");
+            // Add headers from application.properties
+            requestTemplate.header("Origin", origin);
+            requestTemplate.header("X-Requested-With", xRequestedWith);
 
-            logger.info("🚀 Feign Request Headers:");
+            logger.info("Feign Request Headers:");
             requestTemplate.headers().forEach((key, value) ->
                     logger.info("{}: {}", key, value)
             );
         };
-    }
-    @Bean
-    public feign.Logger.Level feignLoggerLevel() {
-        return feign.Logger.Level.FULL;
-    }
-
-    @Bean
-    public OkHttpClient client() {
-        return new OkHttpClient();
     }
 }
